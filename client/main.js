67d1fb419151fb7f897c0330501e277a1f42f7c2
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2020-12-09 17:46:06 +0000

    Tidying up the code
    Just some tidying up in all the three main files, and more explanation about what is hapenning

--- client/main.js
+++ client/main.js
@@ -2,45 +2,29 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
-/*
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
-*/
+/* First of all the routing */
+		/* The route for the layout template */
 Router.configure({
   layoutTemplate: 'layout'
 });
+		/* The route for the mainpage */
 Router.route('/', {
   name: 'main',
   template: 'main'
 });
 
 
-/* First to rename the function names */
+/* Code for the layout (in relation to the main ID !!!) */
+	/* First to rename the function names */
 window.openNav= openNav;
 window.closeNav= closeNav;
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+/* Now the openNav function */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+/* Now the closeNav Function */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0px";
   document.getElementById("main").style.marginLeft = "0px";
