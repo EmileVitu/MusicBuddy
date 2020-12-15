09fb77ed8d4f913f79eab97164bb83c08bc8a771
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2020-12-15 19:24:27 +0000

    Transition

--- client/main.js
+++ client/main.js
@@ -50,9 +50,10 @@ window.closeNav= closeNav;
 
 /*Template.layout.event({"click.openbtn":function(event){});*/
 /* Now the openNav function */
-function openNav() {document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+function openNav() {
+	document.getElementById("mySidenav").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
   
 
